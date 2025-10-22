5b9cec9015341e6c3ef89a35d0e38d82e3d06e5f
Anthony Ostia <meximanianace@gmail.com>
2025-10-22 16:06:11 -0700

    setup cloudflare database and s3

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,5 +1,5 @@
 from src.commands.yt_to_mp3.file import setup_yt_to_mp3
-from src.commands.soundboard.file import setup_soundboard
+from src.commands.soundboard.file import autocomplete_sound_name, setup_soundboard
 from .leave import setup_leave
 from discord import Interaction, app_commands
 
@@ -12,9 +12,6 @@ def setup_commands(tree: app_commands.CommandTree):
     )
     async def file(interaction: Interaction, url: str):
         await setup_yt_to_mp3(interaction, url)
-
-    @tree.command(name="soundboard", description="Play a sound in your voice channel.")
-    @app_commands.describe(sound_name="Select a sound to play")
-    async def file(interaction: Interaction, sound_name: str):
-        await setup_soundboard(interaction, sound_name)
+    
+    setup_soundboard(tree)
 
