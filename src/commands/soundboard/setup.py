01e7a22629df432ef4b247c2f306edacf59fd7b4
Anthony Ostia <44576266+maroonedace@users.noreply.github.com>
2025-12-15 20:43:29 -0800

    update commands

--- src/commands/soundboard/setup.py
+++ src/commands/soundboard/setup.py
@@ -14,12 +14,12 @@ def setup_soundboard(tree: app_commands.CommandTree):
     async def soundboard_play(interaction: Interaction, sound_name: str):
         await setup_soundboard_play(interaction, sound_name)
     
-    @tree.command(name="soundboard_add", description="Add a sound to the soundboard.")
+    @tree.command(name="soundboard-add", description="Add a sound to the soundboard.")
     @app_commands.describe(sound_name="Sound name", sound_file="Sound file")
     async def soundboard_add(interaction: Interaction, sound_name: str, sound_file: discord.Attachment):
         await setup_soundboard_add(interaction, sound_name, sound_file)
     
-    @tree.command(name="soundboard_delete", description="Add a sound to the soundboard.")
+    @tree.command(name="soundboard-delete", description="Add a sound to the soundboard.")
     @app_commands.describe(sound_name="Sound name")
     async def soundboard_delete(interaction: Interaction, sound_name: str):
         await setup_soundboard_delete(interaction, sound_name)
